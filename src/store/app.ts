67ceefe0bb3ccf56945144726ca45358dbeb1809
Azin <zhuye13974604062@163.com>
2023-02-05 19:25:29 +0800

    feat: sidebar header

--- src/store/app.ts
+++ src/store/app.ts
@@ -5,6 +5,7 @@ export const useAppStore = defineStore({
   state: () => ({
     collapse: false,
     isMobile: false,
+    title: 'V-Acg'
   }),
   getters: {
   },
--- src/views/Aside.vue
+++ src/views/Aside.vue
@@ -5,17 +5,35 @@
       'w-[var(--sidebar-width)]': !appStore.collapse,
       'w-[var(--sidebar-width-collapse)]': appStore.collapse && !appStore.isMobile,
       'w-0': appStore.collapse && appStore.isMobile,
-      'fixed': appStore.isMobile,
-      'sticky': !appStore.isMobile
+      fixed: appStore.isMobile,
+      sticky: !appStore.isMobile,
     }"
   >
-  <div class="h-[var(--header-height)] left-0 top-0 bg-red-200">
-  </div>
-  <div class="bg-red-300 overflow-y-auto h-[calc(100vh-var(--header-height))] pb-4rem">
-    <div class="h-800px bg-gray-600"></div>
-  </div>
-  <div class="h-4rem bg-red-400 z-1 absolute left-0 bottom-0 w-full">
-  </div>
+    <!-- sidebar header -->
+    <div class="h-[var(--header-height)] left-0 top-0 flex">
+      <div class="w-[var(--sidebar-width-collapse)] flex items-center justify-center">
+        <RouterLink :to="{ name: 'Root' }"><img src="@/assets/vue.svg" alt="logo" /></RouterLink>
+      </div>
+      <div
+        class="flex-1 flex items-center text-xl lg:text-2xl font-semibold ml-1 truncate"
+        v-show="!appStore.collapse"
+      >
+        <RouterLink :to="{ name: 'Root' }"
+          ><span>{{ appStore.title }}</span></RouterLink
+        >
+      </div>
+    </div>
+    <!-- sidebar header end -->
+
+    <!-- sidebar menu -->
+    <div class="bg-red-300 overflow-y-auto h-[calc(100vh-var(--header-height))] pb-4rem">
+      <div class="h-800px bg-gray-600"></div>
+    </div>
+    <!-- sidebar menu end -->
+
+    <!-- sidebar anchor -->
+    <div class="h-4rem bg-red-400 z-1 absolute left-0 bottom-0 w-full"></div>
+    <!-- sidebar anchor end -->
   </aside>
 </template>
 <script setup lang="ts">
@@ -25,6 +43,6 @@ const appStore = useAppStore()
 </script>
 <style scoped>
 aside {
-  transition: width 300ms ease;
+  transition: all 300ms ease;
 }
 </style>
