afd9ba062ed8105dc3b791672eeb16fe0c836ac2
Azin <zhuye13974604062@163.com>
2023-02-14 23:09:26 +0800

    ci: config github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import { resolve } from 'path'
 import Icons from 'unplugin-icons/vite'
-
 
 const root = process.cwd()
 function pathResolve(dir: string) {
@@ -11,20 +10,26 @@ function pathResolve(dir: string) {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    WindiCSS(),
-    Icons({
-      autoInstall: true,
-    })
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /\@\//,
-        replacement: `${pathResolve('src')}/`
-      }
-    ]
+export default defineConfig(({ command, mode }) => {
+
+  const viteEnv = loadEnv(mode, process.cwd())
+  const { VITE_BASE_PATH } = viteEnv
+  return {
+    base: VITE_BASE_PATH,
+    plugins: [
+      vue(),
+      WindiCSS(),
+      Icons({
+        autoInstall: true,
+      }),
+    ],
+    resolve: {
+      alias: [
+        {
+          find: /\@\//,
+          replacement: `${pathResolve('src')}/`,
+        },
+      ],
+    },
   }
 })
