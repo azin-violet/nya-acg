0b0720146e77a42b2a0015e4f604aedce9b4b248
Azin <zhuye13974604062@163.com>
2023-03-02 11:20:05 +0800

    fix: feedback page size fit in small screen

--- src/views/Feedback.vue
+++ src/views/Feedback.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex items-center justify-center">
-    <Tooltip :content="tip" direction="bottom"><div class="text-2xl w-100 text-center">投稿 & 反馈</div></Tooltip>
+    <Tooltip :content="tip" direction="bottom"><div class="text-2xl text-center">投稿 & 反馈</div></Tooltip>
   </div>
 </template>
 <script setup lang="ts">
